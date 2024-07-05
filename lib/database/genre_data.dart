0b03ef13501a68a31fb4c40b71d383037d2b2ed6
tranminhthinh1204 <tranminhthinh1204@gmail.com>
2024-07-05 19:19:28 +0700

    update home

--- lib/database/genre_data.dart
+++ lib/database/genre_data.dart
@@ -3,11 +3,11 @@
 import '../models/genre.dart';
 
 List<Genre> sampleGenres = [
-  Genre(id: '1', name: 'Action'),
-  Genre(id: '2', name: 'Comedy'),
-  Genre(id: '3', name: 'Drama'),
-  Genre(id: '4', name: 'Sci-Fi'),
-  Genre(id: '5', name: 'Horror'),
-  Genre(id: '6', name: 'Cartoon'),
+  Genre(id: '1', name: 'Phim Đang Chiếu'),
+  Genre(id: '2', name: 'Phim Sắp Chiếu'),
+  Genre(id: '3', name: 'Hành Động'),
+  Genre(id: '4', name: 'Hài Kịch'),
+  Genre(id: '5', name: 'Kinh Dị'),
+  Genre(id: '6', name: 'Hoạt Hình'),
   // Thêm các genre khác tại đây nếu cần
 ];
--- lib/database/movie_data.dart
+++ lib/database/movie_data.dart
@@ -10,22 +10,4 @@ final List<Movie> sampleMovies = [
     description: 'Mô tả chi tiết cho movie 1',
     imageUrl: 'https://via.placeholder.com/150',
   ),
-  Movie(
-    id: '2',
-    title: 'Movie 2',
-    genres: ['Comedy'],
-    // price: '89.000 vnd',
-    Director: 'Đạo diễn 2',
-    description: 'Mô tả chi tiết cho movie 2',
-    imageUrl: 'https://via.placeholder.com/150',
-  ),
-  Movie(
-    id: '3',
-    title: 'Movie 3',
-    genres: ['Horror','Comedy'],
-    // price: '96.000 vnd',
-    Director: 'Đạo diễn 3',
-    description: 'Mô tả chi tiết cho movie 3',
-    imageUrl: 'https://via.placeholder.com/150',
-  ),
 ];
